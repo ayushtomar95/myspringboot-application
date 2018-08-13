74821dbf7b910c2ef9b8a99a1bb4927362e9641f
Ayush Tomar <ayush.tomar@walmartlabs.com>
2018-08-13 14:56:19 +0530

    added git ignore

---
+++ src/main/java/com/random/name/test/MySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.random.name.test;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootApplication {
+
+  public static void main(String[] args) {
+
+  }
+
+
+}
